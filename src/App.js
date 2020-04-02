0cce04028a75d2c41e2437ced1d3225ec469a678
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-04-02 13:36:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      I am being rendered on the Client.
+      <br />
+      <br />
+      So all the hard work is done by the browser and the device.
+      <br />
+      <br />
+      Imagine that on small CPUs and slow networks, is a party pooper.
     </div>
   );
 }
